e590cff3bed2aac2b57fef6a5e8927866bc708e8
Jinhuo Liang <jinhuol@hotmail.com>
2018-10-17 00:03:17 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SelectionListViewMVVM
 Building a User Control with User Defined Order Selection List in WPF and MVVM
+
+This user control supports both un-selecting and re-ordering single and multiple items in the ListView controls. This user control is implemented in WPF and MVVM pattern.
---
+++ SelectionListViewMVVMTests/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Collections.ObjectModel;
+using SelectionListViewMVVM;
+
+namespace SelectionListViewMVVMTests
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetListForUpDownListViewWithMVVM();
+        }
+
+        public UpDownListViewViewModel MvvmViewModel { set; get; }
+
+        /// <summary>
+        /// Set the listed items for the UpDownListView in WPF with Prism in  MVVM
+        /// </summary>
+        private void SetListForUpDownListViewWithMVVM()
+        {
+            // Create a list of items to be displayed in the UpDownListView control.
+            List<Object> objects = new List<Object>
+                {
+                    new DataObjectForTesting("Item 1"),
+                    new DataObjectForTesting("Item 2"),
+                    new DataObjectForTesting("Item 3"),
+                    new DataObjectForTesting("Item 4"),
+                    new DataObjectForTesting("Item 5"),
+                    new DataObjectForTesting("Item 6"),
+                    new DataObjectForTesting("Item 7"),
+                    new DataObjectForTesting("Item 8"),
+                    new DataObjectForTesting("Item 9"),
+                    new DataObjectForTesting("Item 10")
+                };
+
+            // Link the DataContext to the view model.
+            MvvmViewModel = new UpDownListViewViewModel(objects);
+            upDownListViewMVVM.DataContext = MvvmViewModel;
+            listBoxForMVVM.DataContext = MvvmViewModel;
+        }
+    }
+}
